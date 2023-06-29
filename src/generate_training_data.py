4ec5bdf04de9aeb104a9f824fdc99cb0a5ca6457
Bart Schilperoort <b.schilperoort@esciencecenter.nl>
2023-06-29 13:24:10 +0200

    Add docstrings to generate_training_data

--- src/generate_training_data.py
+++ src/generate_training_data.py
@@ -9,6 +9,23 @@ def extract_per_site_era5_data(
     era5_data_folder: Path,
     output_folder: Path,
 ) -> None:
+    """Extract ERA5 data from all gidcells matching each flux site, and write to file.
+
+    The ERA5 data will be matched with the longitude and latitude of each Ameriflux
+    site, and the data corresponding to this cell will be extracted and written to file.
+
+    Data is written as netCDF files to the specified output folder, and is split up
+    per site and per variable. E.g., "ERA5_sp_US-ARM.nc" will contain the ERA5 surface
+    pressure for the US-ARM site.
+    If a file already exists, it is skipped. This allows for efficiently adding
+    additional variables without recomputing everything.
+
+    Args:
+        preprocessed_ameriflux_data: Preprocessed Ameriflux dataset, containing the
+            sites as a dimension.
+        era5_data_folder: Folder containing the unprocessed ERA5 netCDF files.
+        output_folder: Folder to which the extracted data should be written to.
+    """
     ds_sites = xr.open_dataset(
         preprocessed_ameriflux_data,
         chunks={"time": -1, "site": 1},
@@ -27,13 +44,11 @@ def extract_per_site_era5_data(
             latitude=ds_site["latitude"].values,
             longitude=ds_site["longitude"].values,
             method="nearest",
-            tolerance=1
+            tolerance=1,
         )
 
         for era5_var in era5_variables:
-            fpath = (
-                output_folder / f"ERA5_{era5_var}_{ds_site['site'].values[0]}.nc"
-            )
+            fpath = output_folder / f"ERA5_{era5_var}_{ds_site['site'].values[0]}.nc"
 
             if not fpath.exists():
                 print(
@@ -46,7 +61,7 @@ def extract_per_site_era5_data(
                 ds_era5_var = ds_era5_site[[era5_var]]
                 ds_era5_var = ds_era5_var.drop(["latitude", "longitude"])
                 ds_era5_var = ds_era5_var.expand_dims("site")
-                ds_era5_var = ds_era5_var.isel(latitude=0, longitude=0)  
+                ds_era5_var = ds_era5_var.isel(latitude=0, longitude=0)
                 ds_era5_var["site"] = (("site",), ds_site["site"].values)
                 ds_era5_var.to_netcdf(fpath)
             else:
@@ -62,6 +77,15 @@ def combine_era5_fluxnet(
     preprocessed_ameriflux_data: Path,
     preprocessed_era5_folder: Path,
 ) -> xr.Dataset:
+    """Combine the preprocessed ERA5 and Ameriflux datasets into a single dataset.
+
+    Args:
+        preprocessed_ameriflux_data: Preprocessed Ameriflux dataset
+        preprocessed_era5_folder: Preprocessed ERA5 dataset
+
+    Returns:
+        The two datasets merged into a single dataset.
+    """
     ds_era5 = xr.open_mfdataset(
         preprocessed_era5_folder.glob("*.nc"),
         parallel=True,
@@ -78,4 +102,12 @@ def combine_era5_fluxnet(
 def generate_training_df(
     merged_dataset: xr.Dataset,
 ) -> pd.DataFrame:
+    """Generate a dataframe ready for machine learning, from the merged dataset.
+
+    Args:
+        merged_dataset: Merged ERA5 and Ameriflux datasets.
+
+    Returns:
+        Tidy dataframe with only "time" as index.
+    """
     return merged_dataset.to_dataframe().dropna().reset_index().set_index("time")
