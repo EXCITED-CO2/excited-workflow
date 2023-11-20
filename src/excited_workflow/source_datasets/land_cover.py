837a8fb18f4fcd49e4df8a4cc907f713f023d739
Claire Donnelly <claire.b.donnelly@gmail.com>
2023-11-20 14:37:23 +0000

    Added preprocessing step

--- src/excited_workflow/source_datasets/land_cover.py
+++ src/excited_workflow/source_datasets/land_cover.py
@@ -5,6 +5,8 @@ import numpy as np
 import xarray as xr
 import xarray_regrid  # noqa: F401
 
+from pathlib import Path
+
 from excited_workflow.source_datasets.protocol import DataSource
 from excited_workflow.source_datasets.protocol import get_freq_kw
 
@@ -27,11 +29,47 @@ class LandCover(DataSource):
     name: str = "copernicus_landcover"
     variable_names: list[str] = ["lccs_class"]
 
+    def makedir(self):
+        data_path = str(Path(self.get_path()))  + "/preprocessing"
+        Path(data_path).mkdir(parents=True, exist_ok=True)
+
+        return data_path
+    
+
+    def preprocess(
+        self,
+        variables: list[str] | None = None,
+        target_grid: xr.Dataset | None = None,
+    ) -> xr.Dataset:
+        
+        self.validate_variables(variables)
+
+        files = list(self.get_path().glob("*.nc"))
+        if len(files) == 0:
+            msg = f"No netCDF files found at path '{self.get_path()}'"
+            raise FileNotFoundError(msg)
+        
+        new_path = self.makedir()
+
+        for f in files: 
+            ds = xr.open_dataset(f)
+            name = str(Path(f).stem)
+            print(name)
+            ds = ds[["lccs_class"]]  # Only take the class variable.
+            ds = ds.sortby(["lat", "lon"])
+            ds = ds.rename({"lat": "latitude", "lon": "longitude"})
+
+            if variables is not None:
+                ds = ds[variables]
+            if target_grid is not None:
+                ds = ds.regrid.most_common(target_grid, time_dim="time")
+            ds.to_netcdf(new_path + "/" + name + ".nc")
+
+
     def load(
         self,
         freq: Literal["hourly", "monthly"],
         variables: list[str] | None = None,
-        target_grid: xr.Dataset | None = None,
     ) -> xr.Dataset:
         """Load variables from this data source and regrid them to the target grid.
 
@@ -55,16 +93,6 @@ class LandCover(DataSource):
         time_coords = _cftime_to_datetime(ds["time_bounds"].mean(dim="bounds"))
         ds = ds.drop("time_bounds")
         ds["time"] = time_coords
-
-        ds = ds[["lccs_class"]]  # Only take the class variable.
-        ds = ds.sortby(["lat", "lon"])
-        ds = ds.rename({"lat": "latitude", "lon": "longitude"})
-        
-        if variables is not None:
-            ds = ds[variables]
-        if target_grid is not None:
-            ds = ds.isel(time=slice(0, 2))
-            ds = ds.regrid.most_common(target_grid, time_dim="time",  max_mem=1e9)
         
         freq_kw = get_freq_kw(freq)
         ds = ds.resample(time=freq_kw).interpolate("nearest")
