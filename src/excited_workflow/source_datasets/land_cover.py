952fccaf42a842f5d692f59d60e0da9893e895de
Claire Donnelly <claire.b.donnelly@gmail.com>
2023-11-21 21:21:25 +0000

    Added check file exists and added to workflow

--- src/excited_workflow/source_datasets/land_cover.py
+++ src/excited_workflow/source_datasets/land_cover.py
@@ -22,54 +22,106 @@ def _cftime_to_datetime(data: xr.DataArray) -> np.ndarray:
     """
     return np.array([np.datetime64(el) for el in data.to_numpy()])
 
-
 class LandCover(DataSource):
     """Copernicus land cover dataset."""
 
     name: str = "copernicus_landcover"
     variable_names: list[str] = ["lccs_class"]
 
-    def makedir(self):
+    def makedir(self) -> str:
+        """Create preprocessing directory if it does not already exist.
+
+        Returns:
+            String of preprocess directory path
+        """
+
         data_path = str(Path(self.get_path()))  + "/preprocessing"
         Path(data_path).mkdir(parents=True, exist_ok=True)
 
         return data_path
     
-
-    def preprocess(
+    def regrid(        
         self,
+        file,
         variables: list[str] | None = None,
         target_grid: xr.Dataset | None = None,
     ) -> xr.Dataset:
+        """Regrids one file to target dataset.
+
+        Args: 
+            file
+            variables: List of variable names which should be downloaded.
+            target_grid: Grid to which the data should be regridded to.
+
+        Returns:
+            Regridded Dataset.
+        """
         
+        ds = xr.open_dataset(file, chunks={"lat": 2000, "lon": 2000})
+
+        # Set time to middle of bounds.
+        time_coords = _cftime_to_datetime(ds["time_bounds"].mean(dim="bounds"))
+        ds = ds.drop("time_bounds")
+        ds["time"] = time_coords
+
+        ds = ds[["lccs_class"]]  # Only take the class variable.
+        ds = ds.sortby(["lat", "lon"])
+        ds = ds.rename({"lat": "latitude", "lon": "longitude"})
+
+        if variables is not None:
+            ds = ds[variables]
+        if target_grid is not None:
+            ds = ds.regrid.most_common(target_grid, time_dim="time")
+
+        return ds
+
+
+    def preprocess(
+        self,
+        process_path: str,
+        variables: list[str] | None = None,
+        target_grid: xr.Dataset | None = None,
+    ) -> xr.Dataset:
+        """Load variables from this data source and regrid them to the target grid
+        and save to preprocessing directory. 
+
+        Args:
+            variables: List of variable names which should be downloaded.
+            target_grid: Grid to which the data should be regridded to.
+
+        Returns:
+            Preprocessed regridded netcdf files.
+        """
+
         self.validate_variables(variables)
 
         files = list(self.get_path().glob("*.nc"))
         if len(files) == 0:
             msg = f"No netCDF files found at path '{self.get_path()}'"
             raise FileNotFoundError(msg)
-        
-        new_path = self.makedir()
 
         for f in files: 
-            ds = xr.open_dataset(f)
-            name = str(Path(f).stem)
-            print(name)
-            ds = ds[["lccs_class"]]  # Only take the class variable.
-            ds = ds.sortby(["lat", "lon"])
-            ds = ds.rename({"lat": "latitude", "lon": "longitude"})
+            name = process_path + "/" + str(Path(f).stem) + ".nc"
 
-            if variables is not None:
-                ds = ds[variables]
-            if target_grid is not None:
-                ds = ds.regrid.most_common(target_grid, time_dim="time")
-            ds.to_netcdf(new_path + "/" + name + ".nc")
+            if Path(name).is_file() and Path(name).stat().st_size > 0:
+                dat = xr.open_dataset(name)
+                if dat.latitude.size == target_grid.latitude.size and dat.longitude.size == target_grid.longitude.size:
+                    pass
+                else:
+                    print("Not same size")
+                    dat.close()
+                    ds = self.regrid(f, variables, target_grid)
+                    ds.to_netcdf(name)
+            else:
+                ds = self.regrid(f, variables, target_grid)
+                ds.to_netcdf(name)
 
 
     def load(
         self,
         freq: Literal["hourly", "monthly"],
         variables: list[str] | None = None,
+        target_grid: xr.Dataset | None = None,
     ) -> xr.Dataset:
         """Load variables from this data source and regrid them to the target grid.
 
@@ -83,18 +135,16 @@ class LandCover(DataSource):
         """
         self.validate_variables(variables)
 
-        files = list(self.get_path().glob("*.nc"))
+        path = self.makedir()
+        self.preprocess(path, variables, target_grid)
+
+        files = list(Path(path).glob("*.nc"))
         if len(files) == 0:
-            msg = f"No netCDF files found at path '{self.get_path()}'"
+            msg = f"No netCDF files found at path '{path}'"
             raise FileNotFoundError(msg)
-            
-        ds = xr.open_mfdataset(files, chunks={"lat": 2000, "lon": 2000})
-        # Set time to middle of bounds.
-        time_coords = _cftime_to_datetime(ds["time_bounds"].mean(dim="bounds"))
-        ds = ds.drop("time_bounds")
-        ds["time"] = time_coords
-        
+
         freq_kw = get_freq_kw(freq)
+        ds = xr.open_mfdataset(files, chunks={"lat": 2000, "lon": 2000})
         ds = ds.resample(time=freq_kw).interpolate("nearest")
         
         return ds
