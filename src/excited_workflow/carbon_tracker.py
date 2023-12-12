4a89ac89f4bbd3a4c258d32114a770b0bd988daf
Claire Donnelly <claire.b.donnelly@gmail.com>
2023-12-12 15:47:03 +0000

    renaming and splitting save_model function

--- src/excited_workflow/carbon_tracker.py
+++ src/excited_workflow/carbon_tracker.py
@@ -18,7 +18,7 @@ from excited_workflow.source_datasets import datasets
 
 
 def mask_region(
-    regions: Path, target: Path, mask: str, ds_input: xr.Dataset
+    regions: Path, target: Path, ds_input: xr.Dataset, mask: int,
 ) -> xr.Dataset:
     """Limit data to a region and time slice.
 
@@ -37,30 +37,31 @@ def mask_region(
     ds_merged = xr.merge(
         [
             ds_cb[["bio_flux_opt"]],
-            ds_regions[mask],
+            ds_regions["transcom_regions"],
             ds_input,
         ]
     )
 
     ds_sel = ds_merged.sel({"time": slice("2000-01", "2019-12")})
     ds_sel = ds_sel.compute()
-    ds_sel = ds_sel.where(ds_merged[mask] == 2)
+    ds_sel = ds_sel.where(ds_merged["transcom_regions"] == mask)
+    df = ds_sel.to_dataframe().dropna()
 
-    return ds_sel
+    return df
 
 
-def create_groups(ds: xr.Dataset, number: int) -> pd.DataFrame:
+def create_groups(df: pd.DataFrame, number: int) -> pd.DataFrame:
     """Create dataframe with different groups.
 
     Args:
-        ds: Dataset to split into groups.
+        df: Dataframe to split into groups.
         number: number of groups.
 
     Returns:
         Dataframe with groups column.
     """
-    df_train = ds.to_dataframe().dropna()
-    splits = np.array_split(df_train, number)
+    #df_train = ds.to_dataframe().dropna()
+    splits = np.array_split(df, number)
     for i in range(len(splits)):
         splits[i]["group"] = i
     df_train = pd.concat(splits)
@@ -71,7 +72,7 @@ def train_model(df: pd.DataFrame, x_keys: list[str], y_key: str) -> tuple[Any, A
     """Train model.
 
     Args:
-        df: dataframe with training data.
+        df: dataframe for training data.
         x_keys: list of input variables.
         y_key: target variable name.
 
@@ -82,7 +83,7 @@ def train_model(df: pd.DataFrame, x_keys: list[str], y_key: str) -> tuple[Any, A
 
     pycs = pycaret.regression.setup(df_reduced, target=y_key, verbose=False)
     model = pycs.compare_models(
-        include=["lightgbm"], n_select=1, round=7, cross_validation=False
+        include=["lightgbm"], n_select=1, round=5, cross_validation=False
     )
 
     return pycs, model
@@ -158,25 +159,25 @@ def create_scatterplot(prediction: xr.DataArray, target: xr.DataArray) -> None:
 
 
 def validate_model(
-    ds: xr.Dataset, groups: int, x_keys: list[str], y_key: str, output_dir: Path
+    df: pd.DataFrame, groups: int, x_keys: list[str], y_key: str, output_dir: Path
 ) -> None:
     """Validate the trained model by calculating rmse and scatterplots.
 
     Args:
-        ds: dataset for training.
+        df: dataframe for training.
         groups: number of groups.
         x_keys: list of input variables.
         y_key: target variable name.
         output_dir: directory to output rmse and scatterplots.
     """
-    df_group = create_groups(ds, groups)
+    df_group = create_groups(df, groups)
 
     model_vars = [
-        f"{var}: {ds[var].attrs['long_name']}" if "long_name" in ds[var].attrs else var
+        f"**{var}**: {df[var].attrs['long_name']}" if "long_name" in df[var].attrs else var
         for var in x_keys
     ]
 
-    text = "## Carbon tracker model \n \n ### Model variables: \n"
+    text = "## Carbon tracker model \n \n### Model variables (in order): \n\n"
 
     for item in model_vars:
         text = text + f"- {item} \n"
@@ -195,7 +196,7 @@ def validate_model(
         plt.close()
         text = (
             text
-            + f"\n ### Validation plots for group {group} \n"
+            + f"\n### Validation plots for group {group} \n"
             + f"**RMSE map** \n ![image]({output_dir}/rmseplot{group}.png) \n"
             + f"**Scatter plot** \n ![image]({output_dir}/scatter{group}.png) \n"
         )
@@ -204,18 +205,14 @@ def validate_model(
         file.write(text)
 
 
-def save_model(ds: xr.Dataset, x_keys: list[str], y_key: str, output_dir: Path) -> None:
+def save_model(pycs, model, output_dir: Path) -> None:
     """Create lightgbm model for whole dataset and save with ONNX.
 
     Args:
-        ds: dataset used for model creation.
-        x_keys: list of input variables.
-        y_key: target variable name.
+        pycs: 
+        model: 
         output_dir: path to output directory.
     """
-    df = ds.to_dataframe().dropna()
-
-    pycs, model = train_model(df, x_keys, y_key)
     x_test = pycs.get_config("X_test").to_numpy()
 
     lightgbm_onnx = onnxmltools.convert_lightgbm(
@@ -230,8 +227,8 @@ if __name__ == "__main__":
     client = Client()
 
     cb_file = Path("/data/volume_2/EXCITED_prepped_data/CT2022.flux1x1-monthly.nc")
-    regions_path = Path("/data/volume_2/EXCITED_prepped_data/regions.nc")
-    output_path = Path("/home/cdonnelly")
+    regions_file = Path("/data/volume_2/EXCITED_prepped_data/regions.nc")
+    output_path = Path.home()
 
     desired_data = [
         "biomass",
@@ -270,6 +267,7 @@ if __name__ == "__main__":
             for name in desired_data
         ]
     )
-    ds_na = mask_region(regions_path, cb_file, "transcom_regions", ds_input)
-    validate_model(ds_na, 5, x_keys, y_key, output_dir)
-    save_model(ds_na, x_keys, y_key, output_dir)
+    df_na = mask_region(regions_file, cb_file, ds_input, 2)
+    validate_model(df_na, 5, x_keys, y_key, output_dir)
+    pycs, model = train_model(df_na, x_keys, y_key)
+    save_model(pycs, model, output_dir)
