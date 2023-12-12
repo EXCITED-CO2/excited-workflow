d1a171fec2414f3b25618fc759f9b5f00503fb61
Claire Donnelly <claire.b.donnelly@gmail.com>
2023-12-12 09:47:37 +0000

    Changed how output_dir is created

--- src/excited_workflow/carbon_tracker.py
+++ src/excited_workflow/carbon_tracker.py
@@ -17,28 +17,6 @@ import excited_workflow
 from excited_workflow.source_datasets import datasets
 
 
-def merge_datasets(desired_data: list[str], target: Path) -> xr.Dataset:
-    """Merge datasets onto one common grid.
-
-    Args:
-        desired_data: list of desired datasets
-        target: target grid dataset
-
-    Returns:
-        Dataset of input variables.
-    """
-    ds_cb = xr.open_dataset(target)
-    ds_cb = excited_workflow.utils.convert_timestamps(ds_cb)
-    ds_input = xr.merge(
-        [
-            datasets[name].load(freq="monthly", target_grid=ds_cb)
-            for name in desired_data
-        ]
-    )
-
-    return ds_input
-
-
 def mask_region(
     regions: Path, target: Path, mask: str, ds_input: xr.Dataset
 ) -> xr.Dataset:
@@ -90,11 +68,22 @@ def create_groups(ds: xr.Dataset, number: int) -> pd.DataFrame:
 
 
 def train_model(df: pd.DataFrame, x_keys: list[str], y_key: str) -> tuple[Any, Any]:
-    """Train model."""
+    """Train model.
+
+    Args:
+        df: dataframe with training data.
+        x_keys: list of input variables.
+        y_key: target variable name.
+
+    Returns:
+        trained model.
+    """
     df_reduced = df[x_keys + [y_key]]
 
     pycs = pycaret.regression.setup(df_reduced, target=y_key, verbose=False)
-    model = pycs.compare_models(include=["lightgbm"], n_select=1, round=7)
+    model = pycs.compare_models(
+        include=["lightgbm"], n_select=1, round=7, cross_validation=False
+    )
 
     return pycs, model
 
@@ -169,7 +158,7 @@ def create_scatterplot(prediction: xr.DataArray, target: xr.DataArray) -> None:
 
 
 def validate_model(
-    ds: xr.Dataset, groups: int, x_keys: list[str], y_key: str, output_path: Path
+    ds: xr.Dataset, groups: int, x_keys: list[str], y_key: str, output_dir: Path
 ) -> None:
     """Validate the trained model by calculating rmse and scatterplots.
 
@@ -178,17 +167,14 @@ def validate_model(
         groups: number of groups.
         x_keys: list of input variables.
         y_key: target variable name.
-        output_path: directory to output rmse and scatterplots.
+        output_dir: directory to output rmse and scatterplots.
     """
-    time = datetime.datetime.now().strftime("%Y-%m-%d_%H")
-    output_dir = output_path / f"carbon_tracker-{time}"
-    output_dir.mkdir(parents=True, exist_ok=True)
-
     df_group = create_groups(ds, groups)
 
-    model_vars = [f"{var}: {ds[var].attrs['long_name']}" 
-                  if "long_name" in ds[var].attrs 
-                  else var for var in x_keys]
+    model_vars = [
+        f"{var}: {ds[var].attrs['long_name']}" if "long_name" in ds[var].attrs else var
+        for var in x_keys
+    ]
 
     text = "## Carbon tracker model \n \n ### Model variables: \n"
 
@@ -218,19 +204,15 @@ def validate_model(
         file.write(text)
 
 
-def save_model(
-    ds: xr.Dataset, x_keys: list[str], y_key: str, output_path: Path
-) -> None:
+def save_model(ds: xr.Dataset, x_keys: list[str], y_key: str, output_dir: Path) -> None:
     """Create lightgbm model for whole dataset and save with ONNX.
 
     Args:
         ds: dataset used for model creation.
         x_keys: list of input variables.
         y_key: target variable name.
-        output_path: path to output directory.
+        output_dir: path to output directory.
     """
-    time = datetime.datetime.now().strftime("%Y-%m-%d_%H")
-    output_dir = output_path / f"carbon_tracker-{time}"
     df = ds.to_dataframe().dropna()
 
     pycs, model = train_model(df, x_keys, y_key)
@@ -243,16 +225,13 @@ def save_model(
     with open(output_dir / "lightgbm.onnx", "wb") as f:
         f.write(lightgbm_onnx.SerializeToString())
 
-    # with open(output_dir / "model_description.md", "a") as file:
-    #    file.write(text)
-
 
 if __name__ == "__main__":
     client = Client()
 
-    ct_path = Path("/data/volume_2/EXCITED_prepped_data/CT2022.flux1x1-monthly.nc")
+    cb_file = Path("/data/volume_2/EXCITED_prepped_data/CT2022.flux1x1-monthly.nc")
     regions_path = Path("/data/volume_2/EXCITED_prepped_data/regions.nc")
-    output_dir = Path("/home/cdonnelly")
+    output_path = Path("/home/cdonnelly")
 
     desired_data = [
         "biomass",
@@ -279,7 +258,18 @@ if __name__ == "__main__":
     ]
     y_key = "bio_flux_opt"
 
-    ds_input = merge_datasets(desired_data, ct_path)
-    ds_na = mask_region(regions_path, ct_path, "transcom_regions", ds_input)
+    time = datetime.datetime.now().strftime("%Y-%m-%d_%H_%M")
+    output_dir = output_path / f"carbon_tracker-{time}"
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    ds_cb = xr.open_dataset(cb_file)
+    ds_cb = excited_workflow.utils.convert_timestamps(ds_cb)
+    ds_input = xr.merge(
+        [
+            datasets[name].load(freq="monthly", target_grid=ds_cb)
+            for name in desired_data
+        ]
+    )
+    ds_na = mask_region(regions_path, cb_file, "transcom_regions", ds_input)
     validate_model(ds_na, 5, x_keys, y_key, output_dir)
     save_model(ds_na, x_keys, y_key, output_dir)
