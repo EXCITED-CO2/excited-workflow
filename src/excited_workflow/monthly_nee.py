1d4de57d1b856e6343a9a534ae63385a647fa4a0
Claire Donnelly <claire.b.donnelly@gmail.com>
2023-12-22 11:02:49 +0000

    Added difference plots and kept time as index

--- src/excited_workflow/monthly_nee.py
+++ src/excited_workflow/monthly_nee.py
@@ -1,8 +1,8 @@
 """Create NEE datasets from CarbonTracker model."""
 from datetime import datetime
 from pathlib import Path
+from typing import Any
 
-import numpy as np
 import pandas as pd
 import xarray as xr
 from dask.distributed import Client
@@ -12,7 +12,7 @@ from onnxruntime import InferenceSession
 client = Client()
 
 
-def run_model(model_dir: Path, df: pd.DataFrame, x_keys: list[str]) -> xr.DataArray:
+def run_model(model_dir: Path, df: pd.DataFrame, x_keys: list[str]) -> Any:
     """Open model and run it.
 
     Args:
@@ -34,7 +34,7 @@ def run_model(model_dir: Path, df: pd.DataFrame, x_keys: list[str]) -> xr.DataAr
 
 def get_predictions(
     ds_input: xr.Dataset, x_keys: list[str], ds_regions: xr.Dataset, model_dir: Path
-):
+) -> list[pd.DataFrame]:
     """Create NEE monthly dataset.
 
     Args:
@@ -47,27 +47,22 @@ def get_predictions(
     ds_merge = xr.merge([dsx, ds_regions["transcom_regions"]])
     allnan = ds_merge.isnull().all(dim=["latitude", "longitude"]).compute()
 
-    predictions = []
-    times = []
-    for idx, dtime in enumerate(ds_merge["time"]):
-        ds_sel = ds_merge.isel(time=idx)
+    dfs = []
+    for idx, _dtime in enumerate(ds_merge["time"]):
+        ds_sel = ds_merge.isel(time=[idx])
         if not any([allnan.isel(time=idx)[var] for var in allnan.data_vars]):
             ds_sel = ds_sel.compute()
             ds_na = ds_sel.where(ds_merge["transcom_regions"] == 2)
             df_sel = ds_na.to_dataframe().dropna()
+            prediction = run_model(model_dir, df_sel, x_keys)
+            dfs.append(
+                pd.DataFrame(data=prediction, index=df_sel.index, columns=["bio_flux"])
+            )
 
-            predictions.append(run_model(model_dir, df_sel, x_keys))
-            times.append(dtime.to_numpy())
-
-    dfs = [
-        pd.DataFrame(data=pred, index=df_sel.index, columns=["bio_flux"])
-        for pred in predictions
-    ]
-
-    return dfs, times
+    return dfs
 
 
-def create_dataset(dfs: list[pd.DataFrame], times: list[str], data_dir: Path) -> None:
+def create_dataset(dfs: list[pd.DataFrame], data_dir: Path) -> Any:
     """Create dataset for predictions over entire time period.
 
     Args:
@@ -80,7 +75,6 @@ def create_dataset(dfs: list[pd.DataFrame], times: list[str], data_dir: Path) ->
     """
     dss = [df.to_xarray().sortby(["latitude", "longitude"]) for df in dfs]
     ds_out = xr.concat(dss, dim="time")
-    ds_out["time"] = np.array(times)
     ds_out["bio_flux"].attrs = {
         "long_name": "terrestrial biosphere CO2 flux",
         "units": "mol m-2 s-1",
@@ -91,6 +85,7 @@ def create_dataset(dfs: list[pd.DataFrame], times: list[str], data_dir: Path) ->
         "institution": "Netherlands eScience Center",
         "history": f"Date created: {datetime.now().strftime('%Y-%m-%d')}",
     }
+    ds_out = xr.Dataset(ds_out)
 
     output_dir = data_dir / "NEE/CarbonTracker"
     output_dir.mkdir(parents=True, exist_ok=True)
@@ -98,5 +93,5 @@ def create_dataset(dfs: list[pd.DataFrame], times: list[str], data_dir: Path) ->
     comp = dict(zlib=True, complevel=8)
     encoding = {var: comp for var in ds_out.data_vars}
     ds_out.to_netcdf(output_dir / "NEE_monthly.nc", encoding=encoding)
-    
+
     return ds_out
