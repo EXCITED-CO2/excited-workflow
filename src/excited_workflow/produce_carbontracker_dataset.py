514266791d81c5254f425e21e0c7add79f9197b7
Claire Donnelly <claire.b.donnelly@gmail.com>
2024-02-29 23:00:51 +0000

    Created produce_dataset function and simplified notebooks.

--- src/excited_workflow/produce_carbontracker_dataset.py
+++ src/excited_workflow/produce_carbontracker_dataset.py
@@ -8,6 +8,8 @@ import xarray as xr
 from onnxruntime import InferenceSession
 
 import excited_workflow
+from excited_workflow.common_utils import read_model_variables
+from excited_workflow.source_datasets import datasets
 
 
 def run_model(onnx_model: Path, df: pd.DataFrame, X_keys: list[str]) -> Any:
@@ -16,7 +18,7 @@ def run_model(onnx_model: Path, df: pd.DataFrame, X_keys: list[str]) -> Any:
     Args:
         onnx_model: path to model file.
         df: input dataframe for running model.
-        x_keys: list of variables required for model.
+        X_keys: list of variables required for model.
 
     Returns:
         Array of predictions.
@@ -31,18 +33,20 @@ def run_model(onnx_model: Path, df: pd.DataFrame, X_keys: list[str]) -> Any:
 
 
 def predict(
-    ds_input: xr.Dataset, X_keys: list[str], ds_regions: xr.Dataset, model_dir: Path
+    ds_merge: xr.Dataset,
+    onnx_model: Path,
+    X_keys: list[str],
 ) -> list[pd.DataFrame]:
     """Create NEE monthly dataset.
 
     Args:
-        ds_input: input dataset.
-        x_keys: list of variables required for model.
-        ds_regions: dataset of regions.
-        model_dir: path to model directory.
+        ds_merge: input dataset.
+        onnx_model: path to model file.
+        X_keys: list of variables required for model.
+
+    Returns:
+        list of dataframes with model predictions.
     """
-    dsx = ds_input[X_keys]
-    ds_merge = xr.merge([dsx, ds_regions["transcom_regions"]])
     allnan = ds_merge.isnull().all(dim=["latitude", "longitude"]).compute()
 
     dfs = []
@@ -53,7 +57,7 @@ def predict(
             ds_na = ds_sel.where(ds_merge["transcom_regions"] == 2)
             ds_land = ds_na.where(ds_na["lccs_class"] != 210)
             df_land = ds_land.to_dataframe().dropna()
-            prediction = run_model(model_dir, df_land, X_keys)
+            prediction = run_model(onnx_model, df_land, X_keys)
             dfs.append(
                 pd.DataFrame(data=prediction, index=df_land.index, columns=["bio_flux"])
             )
@@ -61,17 +65,41 @@ def predict(
     return dfs  #
 
 
-def produce_dataset(dfs: list[pd.DataFrame], data_dir: Path, X_keys: list[str]) -> Any:
+def produce_dataset(
+    model_dir: Path,
+    model_name: str,
+    carbontracker_file: Path,
+    regions_file: Path,
+    data_dir: Path,
+) -> Any:
     """Create dataset for predictions over entire time period.
 
     Args:
-        dfs: list of prediction dataframes.
+        model_dir: path to directory containing onnx model.
+        model_name: name of onnx model.
+        carbontracker_file: path to carbontracker file.
+        regions_file: path to regions file.
         data_dir: path to data directory.
-        x_keys: list of variables required for model.
-
-    Returns:
-        Output NEE dataset.
     """
+    predictors, target, required_datasets = read_model_variables(model_dir)
+    X_keys = list(predictors)  # noqa: N806  (Get the var names only)
+
+    ds_cb = xr.open_dataset(carbontracker_file)
+    ds_cb = excited_workflow.utils.convert_timestamps(ds_cb)
+    ds_regions = xr.open_dataset(regions_file)
+    ds_input = xr.merge(
+        [
+            datasets[name].load(freq="monthly", target_grid=ds_cb)
+            for name in required_datasets
+        ]
+    )
+
+    dsx = ds_input[X_keys]
+    ds_merge = xr.merge([dsx, ds_regions["transcom_regions"]])
+    onnx_model = model_dir / model_name
+
+    dfs = predict(ds_merge, onnx_model, X_keys)
+
     dss = [df.to_xarray().sortby(["latitude", "longitude"]) for df in dfs]
     ds_out = xr.concat(dss, dim="time")
     ds_out["bio_flux"].attrs = {
@@ -95,5 +123,3 @@ def produce_dataset(dfs: list[pd.DataFrame], data_dir: Path, X_keys: list[str]) 
     comp = dict(zlib=True, complevel=8)
     encoding = {var: comp for var in ds_out.data_vars}
     ds_out.to_netcdf(output_dir / "NEE_monthly.nc", encoding=encoding)
-
-    return ds_out
