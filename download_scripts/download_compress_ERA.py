1d9863eb617703f9c5052235157e6a5b51d4a4ca
Bart Schilperoort <b.schilperoort@esciencecenter.nl>
2024-06-20 06:13:34 +0000

    Update download+compress script

--- download_scripts/download_compress_ERA.py
+++ download_scripts/download_compress_ERA.py
@@ -2,12 +2,14 @@
 from itertools import product
 from pathlib import Path
 
+import h5py
+import hdf5plugin
 import cdsapi
 import xarray as xr
-from enstools.io import write
 
+from pathos.threading import ThreadPool as Pool
 
-output_dir = Path("/data/EXCITED/test")
+output_dir = Path("/data/volume_2/hourly_global_era5")
 
 var_names = {
     "2m_temperature": "t2m", # K
@@ -21,26 +23,25 @@ var_names = {
 }
 
 compression = {
-    "t2m": "lossy,sz3,abs,0.1", # 0.1 K
-    "d2m": "lossy,sz3,abs,0.1", # 0.1 K
-    "ssr": "lossy,sz3,abs,0.00028",  # 1 J/hr/m2
-    "str": "lossy,sz3,abs,0.00028",  # 1 J/hr/m2
-    "msshf": "lossy,sz3,abs,1", # 1 W/m2
-    "mslhf": "lossy,sz3,abs,1", # 1 W/m2
-    "tp": "lossy,sz3,abs,1", # 1 Pa
-    "sp": "lossy,sz3,abs,1e-6", # 0.01 mm
+    "t2m": {**hdf5plugin.SZ3(absolute=0.1)}, # 0.1 K
+    "d2m": {**hdf5plugin.SZ3(absolute=0.1)}, # 0.1 K
+    "ssr": {**hdf5plugin.SZ3(absolute=1e3)},  # 1000 J/m2
+    "str": {**hdf5plugin.SZ(absolute=250)},  # 250 J/m2 (sz) sz3 bugs out
+    "msshf": {**hdf5plugin.SZ3(absolute=1)}, # 1 W/m2
+    "mslhf": {**hdf5plugin.SZ3(absolute=1)}, # 1 W/m2
+    "sp": {**hdf5plugin.SZ3(absolute=1)}, # 1 Pa
+    "tp": {**hdf5plugin.SZ3(absolute=1e-6)}, # 0.01 mm
 }
 
 variables = [var for var in var_names]
 years = [str(y) for y in range(2000,2020)]
-months = [str(m) for m in range(1,13)]
+months = [f"{m:02}" for m in range(1,13)]
 days = [f"{d:02}" for d in range(32)]
 hours = [f"{hr:02}:00" for hr in range(0,24)]
 
 c = cdsapi.Client()
 
-for var, year, month in product(variables, years, months):
-    file = output_dir / f"{var}_{year}-{month}.nc"
+def get_data(file, var, year, month):
     c.retrieve(
         "reanalysis-era5-single-levels",
         {
@@ -56,8 +57,27 @@ for var, year, month in product(variables, years, months):
     )
 
     ds = xr.load_dataset(file)
-    write(
-        ds,
-        file_path=file,
-        compression=compression,
+    ds.to_netcdf(
+        file,
+        engine="h5netcdf",
+        encoding=compression,
     )
+
+files = []
+itervars = []
+iteryears = []
+itermonths = []
+
+for var, year, month in product(variables, years, months):
+    file = output_dir / f"{var}_{year}-{month:02}.nc"
+    
+    if not file.exists():
+        files.append(file)
+        itervars.append(var)
+        iteryears.append(year)
+        itermonths.append(month)
+    else:
+        print(f"File {file.name} already exists, skipping...")
+
+pool = Pool(nodes=4)
+pool.map(get_data, files, itervars, iteryears, itermonths)
